c2977effbb8c864ea937eb33ba1794a63940b603
sharto66 <sean.hartnett@student.dit.ie>
2015-11-04 00:16:14 +0000

    Hough and Gaussian testing added

--- Optical-Security-Threat-Detection/ProcessingMethods.cpp
+++ Optical-Security-Threat-Detection/ProcessingMethods.cpp
@@ -9,8 +9,10 @@ using namespace std;
 using namespace cv;
 
 int MAX_KERNEL_LENGTH = 31;
+int GAUSSIAN_KERNEL = 6;
 
-Mat filterImage(Mat src){
+Mat filterImage(Mat src)
+{
     Mat dst;
     for ( int j = 1; j < MAX_KERNEL_LENGTH; j = j + 2 )
     {
@@ -19,13 +21,72 @@ Mat filterImage(Mat src){
     return dst;
 }
 
-Mat blobDetection(Mat src){
-           Mat dst;
-           cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create();
-           std::vector<KeyPoint> keypoints;
-           detector->detect(src, keypoints);
-           drawKeypoints(src, keypoints, dst, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-           return dst;
+Mat blurImage(Mat src)
+{
+    Mat dst;
+    for (int i=1; i<GAUSSIAN_KERNEL; i=i+2)
+    {
+        GaussianBlur(src, dst, Size(i, i), 0, 0);
+    }
+    return dst;
 }
 
+Mat blobDetection(Mat src)
+{
+    Mat dst;
+    SimpleBlobDetector::Params params;
 
+    // Filter by Area.
+    params.filterByArea = true;
+    params.minArea = 300;
+
+    // Filter by Circularity
+    params.filterByCircularity = false;
+    params.minCircularity = 0.5;
+
+    // Filter by Convexity
+    params.filterByConvexity = false;
+    params.maxConvexity = 0.87;
+
+    // Filter by Inertia
+    params.filterByInertia = true;
+    params.minInertiaRatio = 0.01;
+    params.maxInertiaRatio = 0.3;
+    
+    cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+    std::vector<KeyPoint> keypoints;
+    detector->detect(src, keypoints);
+    drawKeypoints(src, keypoints, dst, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    return dst;
+}
+
+Mat barrelDetection(Mat src)
+{
+    Mat dst;
+    cvtColor(src, dst, CV_GRAY2BGR);
+    std::vector<Vec2f> lines;
+    cv::HoughLines(src, lines, 1, CV_PI/180, 70, 0, 10);
+    cout << lines.size() << endl;
+    for(int i = 0; i < lines.size(); i++)
+    {
+        float r = lines[i][0];
+        float theta = lines[i][1];
+        cv::Point p1, p2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*r, y0 = b*r;
+        p1.x = cvRound(x0 + 1000*(-b)); //the first point
+        p1.y = cvRound(y0 + 1000*(a)); //the first point
+        p2.x = cvRound(x0 - 1000*(-b)); //the second point
+        p2.y = cvRound(y0 - 1000*(a)); //the second point
+        line(dst, p1, p2, Scalar(255,100,0), 1, 4, 0);
+    }
+    return dst;
+}
+
+Mat edgeDetection(Mat src)
+{
+    Mat edge, dst;
+    Canny(src, edge, 50, 150, 3);
+    edge.convertTo(dst, CV_8U);
+    return dst;
+}
--- Optical-Security-Threat-Detection/main.cpp
+++ Optical-Security-Threat-Detection/main.cpp
@@ -5,24 +5,26 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#define NUM 5
+#define NUM 10
 
 using namespace std;
 using namespace cv;
 
 Mat filterImage(Mat src);
 Mat blobDetection(Mat src);
+Mat edgeDetection(Mat src);
+Mat barrelDetection(Mat src);
+Mat blurImage(Mat src);
 
 int main()
 {
     Mat images[NUM];
-    Mat image, dst;
-    Mat im_with_keypoints;
+    Mat image;
     char image_name[100];
     
     for(int i=0;i < NUM; i++)
     {
-        sprintf(image_name, "C:\\Users\\Sean\\Pictures\\guns/image%d.jpg",i+1);
+        sprintf(image_name, "C:\\Users\\Sean\\Pictures\\guns\\handguns/image%d.jpg",i+1);
         image = imread(image_name, IMREAD_GRAYSCALE);
         images[i] = image;
     }
@@ -31,15 +33,16 @@ int main()
     for(int i=0;i < size; i++)
     {
         if(images[i].data){
-           images[i] = filterImage(images[i]);
-           images[i] = blobDetection(images[i]);
+           images[i] = blurImage(images[i]);
+           //images[i] = filterImage(images[i]);
+           //images[i] = blobDetection(images[i]);
+           images[i] = edgeDetection(images[i]);
+           images[i] = barrelDetection(images[i]);
            namedWindow(string("Display window") + std::to_string(i+1), WINDOW_AUTOSIZE);
-           imshow(string("Display window") + std::to_string(i+1), images[i] );
+           imshow(string("Display window") + std::to_string(i+1), images[i]);
         }
     }
     waitKey(0);
     cvDestroyAllWindows();
     return 0;
 }
-
-
